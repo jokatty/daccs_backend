e6bca02806c3470c3cde9c84fe0890cdb9bf88c7
jokatty <jyotikattani@gmail.com>
2022-01-21 09:05:22 +0200

    add seed data and send it as response object

--- index.mjs
+++ index.mjs
@@ -1,18 +1,43 @@
 import express from 'express';
+import pg from 'pg';
 
 const app = express();
 const PORT = 3004;
+const { Pool } = pg;
+
+// configure database
+let pgConnectionConfig;
+if (process.env.DATABASE_URL) {
+  pgConnectionConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  pgConnectionConfig = {
+    user: 'jyotikattani',
+    host: 'localhost',
+    database: 'daccs',
+    port: '5432',
+  };
+}
+
+const pool = new Pool(pgConnectionConfig);
 
 // set a home route
 app.get('/', (req, res) => {
-  const userInformation = {
-    name: 'Justus Darley',
-    bankAc: 'NL67 000 111',
-    email: 'jd@example.com',
-    phone: '01234567',
-  };
-  console.log(userInformation);
-  res.send(userInformation);
+  const sqlQuery = 'SELECT * FROM users';
+  pool.query(sqlQuery, (error, result) => {
+    if (error) {
+      console.log('error executing the query', error.stack);
+      res.status(503).send(result.rows);
+    }
+    const userData = {
+      data: result.rows,
+    };
+    res.send(userData);
+  });
 });
 // spin the server in port 3004
 app.listen(PORT, () => {
---
+++ seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO users(user_name, bank_account, user_address, email, phone) VALUES ('Justus Darley', '222000111', 'Amsterdam zuid', 'justus@example.com', '0610020997')
