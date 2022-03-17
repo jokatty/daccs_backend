e298db7c9b1317f0a38331b17a647c88f83d46cf
jokatty <jyotikattani@gmail.com>
2022-03-17 18:27:15 +0100

    change bank a/c data type to varchar

--- init_tables.sql
+++ init_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   user_name TEXT,
-  bank_account INTEGER,
+  bank_account VARCHAR,
   user_address TEXT,
   email TEXT,
   phone INTEGER
