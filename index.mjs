6853688389b824cbe205bdac3159dbe24aa55a74
jokatty <jyotikattani@gmail.com>
2022-03-17 21:13:13 +0100

    remove debugging logs and non fucntional code

--- index.mjs
+++ index.mjs
@@ -6,6 +6,12 @@ const app = express();
 const PORT = 3004;
 const { Pool } = pg;
 
+// Bind Express middleware to parse requests body for POST requests
+app.use(express.urlencoded({ extended: true }));
+
+// read data from our AJAX POST request and populate request.body
+app.use(express.json());
+
 // let the backend know the response is coming from local host
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
@@ -14,6 +20,7 @@ app.use(cors({
   credentials: true,
   origin: FRONTEND_URL,
 }));
+
 // configure database
 let pgConnectionConfig;
 if (process.env.DATABASE_URL) {
@@ -34,7 +41,7 @@ if (process.env.DATABASE_URL) {
 
 const pool = new Pool(pgConnectionConfig);
 
-// set a home route
+// set a home route. Sends user data in the response body.
 app.get('/', (req, res) => {
   const sqlQuery = 'SELECT * FROM users';
   pool.query(sqlQuery, (error, result) => {
@@ -45,6 +52,21 @@ app.get('/', (req, res) => {
       data: result.rows,
     };
     res.send(userData.data);
+  });
+});
+
+// post request route for receiving updated data.
+app.post('/update', (req, res) => {
+  const {
+    id, userName, email, phone, address, bankAccount,
+  } = req.body;
+  const sqlQuery = `UPDATE users SET user_name='${userName}', user_address='${address}', phone='${phone}', email='${email}', bank_account='${bankAccount}' WHERE id=${id}`;
+
+  pool.query(sqlQuery, (error, result) => {
+    if (error) {
+      res.status(503).send(result);
+    }
+    res.send(result);
   });
 });
 // spin the server in port 3004
