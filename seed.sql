e9fd3ee512c6f4fd167f08615bcdaf6dfd15a3e3
jokatty <jyotikattani@gmail.com>
2022-03-17 21:38:35 +0100

    update seed data

--- seed.sql
+++ seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO users(user_name, bank_account, user_address, email, phone) VALUES ('Justus Darley', 'NL67 INGB 1256 1255 27', 'Amsterdam zuid', 'justus@example.com', '0610020997')
+INSERT INTO users(user_name, bank_account, user_address, email, phone) VALUES ('Justus Darley', 'NL67 INGB 1256 1255 00', 'Amsterdam zuid', 'justus@example.com', '0610020997')
